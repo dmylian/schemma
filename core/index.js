0388f5e9c31c1097b048a8a6fd22114470312e50
Daniel <danylo.ukl@gmail.com>
2016-11-05 13:26:08 +0200

    flow-lint added

--- core/index.js
+++ core/index.js
@@ -6,23 +6,23 @@ import type {
 
 
 const get = (p: string): Function => (object: Object): any => object[p];
-const call = (param: any): Function => (fn: Function): any => fn(param);
+const call = (...param: any): Function => (fn: Function): any => fn(...param);
 
-const compose = (...validators: Array<ValidatorFn>): Function | null => {
-  if (!validators.length) return null;
-  return (field: any) => filterErrors(applyValidators(field, validators));
-}
-
-const applyValidators = (field: any, validators: Array<Function>): Array<ValidationResult<*>> => validators.map(call(field));
+const applyValidators = (field: any, validators: Array<ValidatorFn>): Array<ValidationResult<*>> => validators.map(call(field));
 const filterErrors = (errors: Array<ValidationResult<*>>): Array<ValidationResult<*>> => errors.filter(get('info'));
 
+const compose = (...validators: Array<ValidatorFn>): Function | null => {
+  if (!validators.length) {
+    return null;
+  }
+  return (field: any): Array<ValidationResult<*>> => filterErrors(applyValidators(field, validators));
+}
 
-const mergeErrors = (source: Object, rules: Object): Object => {
-  return Object.keys(source).reduce((acc: Object, c: string) => {
+const mergeErrors = (source: Object, rules: Object): Object =>
+  Object.keys(source).reduce((acc: Object, c: string) => {
     acc[c] = rules[c](source[c]);
     return acc;
   }, {});
-}
 
 module.exports = {
   compose,
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
-## Validator
+## Angular2-like composable (kind-of) validator
 
 Just exploring the sources of [Angular2 validator](https://github.com/angular/angular/blob/master/modules/%40angular/forms/src/validators.ts) and [Ramda](https://github.com/ramda/ramda/tree/master/src) utility functions to create custom angular-like validator that supports composition of the validation rules.
+And playing with modern ES ecosystem stuff.
+
+To build, make sure that you have `babel` installed and run `npm install`;
+
+### Roadmap
+- [x] test suites [Jest](https://facebook.github.io/jest/)
+- [x] static types [Flow](https://flowtype.org/)
+- [x] flow-lint
